7c996d5bdb46287dcd8867d8469484f7675d2734
Ilia Orlov <66363651+illfixit@users.noreply.github.com>
2023-08-10 11:46:56 +0200

    Fix fake-backend dependencies, add Typescript support
    * Fix fake-backend dependencies, add Typescript support
    
    * Add fake-backend section in README
    
    ---------
    
    Co-authored-by: Ilia Orlov <ilia.orlov@sovity.de>

--- authority-portal-frontend/fake-backend/serve.ts
+++ authority-portal-frontend/fake-backend/serve.ts
@@ -1,11 +1,11 @@
+import express, { Request, Response } from 'express';
 console.log('Hi!')
 
-const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Typescript World!')
 })
 
 app.listen(port, () => {
