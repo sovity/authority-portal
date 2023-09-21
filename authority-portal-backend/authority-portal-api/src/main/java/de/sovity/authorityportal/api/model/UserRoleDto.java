0e31996f865b71be18bc919d007439b0847f1ef5
Jan Ridderbusch <36418748+jridderbusch@users.noreply.github.com>
2023-09-21 09:51:51 +0200

    Add more dev data

--- authority-portal-backend/authority-portal-api/src/main/java/de/sovity/authorityportal/api/model/UserRoleDto.java
+++ authority-portal-backend/authority-portal-api/src/main/java/de/sovity/authorityportal/api/model/UserRoleDto.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Available user roles.")
 public enum UserRoleDto {
+    OPERATOR_ADMIN,
+    SERVICEPARTNER_ADMIN,
     AUTHORITY_ADMIN,
     AUTHORITY_USER,
     PARTICIPANT_ADMIN,
