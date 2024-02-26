091af307de3f7bdcffc3ca41095a678c5d8afa98
kulgg <75735874+kulgg@users.noreply.github.com>
2024-02-26 13:09:16 +0100

    fix: footer links row display

--- authority-portal-frontend/src/app/shared/components/business/footer-links/footer-links.component.ts
+++ authority-portal-frontend/src/app/shared/components/business/footer-links/footer-links.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, HostBinding, Inject} from '@angular/core';
 import {APP_CONFIG, AppConfig} from 'src/app/core/config/app-config';
 import {FooterLink} from './footer-link.model';
 
@@ -7,6 +7,13 @@ import {FooterLink} from './footer-link.model';
   templateUrl: './footer-links.component.html',
 })
 export class FooterLinksComponent {
+  @HostBinding('class.flex')
+  @HostBinding('class.flex-wrap')
+  @HostBinding('class.justify-center')
+  @HostBinding('class.items-center')
+  @HostBinding('class.gap-2')
+  cls = true;
+
   footerLinks: FooterLink[] = [
     {
       name: 'Privacy Policy',
