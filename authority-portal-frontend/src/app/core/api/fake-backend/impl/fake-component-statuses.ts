8033e0116777e97bc65630c74a05141e90090a12
Richard Treier <richard.treier@sovity.de>
2024-03-05 09:03:23 +0100

    fix: dashboard not reacting to environment switches

--- authority-portal-frontend/src/app/core/api/fake-backend/impl/fake-component-statuses.ts
+++ authority-portal-frontend/src/app/core/api/fake-backend/impl/fake-component-statuses.ts
@@ -25,54 +25,46 @@ const maintenance = (): UptimeStatusDto => ({
   },
 });
 
-const nullStatus = (): UptimeStatusDto => ({
-  componentStatus: 'PENDING',
-  uptimePercentage: 0,
+const down = (): UptimeStatusDto => ({
+  componentStatus: 'DOWN',
+  uptimePercentage: 51.2,
   timeSpan: {
     seconds: 2592000,
   },
-  upSince: {},
+  upSince: {
+    seconds: 0,
+  },
 });
 
-const results: ComponentStatusOverview[] = [
-  {
-    brokerStatus: up(),
-    dapsStatus: up(),
-    loggingHouseStatus: maintenance(),
-    onlineConnectors: 20,
-    disturbedConnectors: 2,
-    offlineConnectors: 1,
-  },
-  {
-    brokerStatus: up(),
-    dapsStatus: up(),
-    loggingHouseStatus: up(),
-    onlineConnectors: 1,
-    disturbedConnectors: 0,
-    offlineConnectors: 0,
-  },
-  {
-    brokerStatus: undefined,
-    dapsStatus: undefined,
-    loggingHouseStatus: undefined,
-    onlineConnectors: 0,
-    disturbedConnectors: 0,
-    offlineConnectors: 0,
-  },
-  {
-    brokerStatus: nullStatus(),
-    dapsStatus: nullStatus(),
-    loggingHouseStatus: nullStatus(),
-    onlineConnectors: 0,
-    disturbedConnectors: 0,
-    offlineConnectors: 0,
-  },
-];
-
-// rotate results
-let i = 0;
-export const getComponentStatus = (): ComponentStatusOverview => {
-  const result = results[i];
-  i = (i + 1) % results.length;
-  return result;
+export const getComponentStatus = (
+  environmentId: string,
+): ComponentStatusOverview => {
+  if (environmentId === 'development') {
+    return {
+      brokerStatus: up(),
+      dapsStatus: maintenance(),
+      loggingHouseStatus: down(),
+      onlineConnectors: 20,
+      disturbedConnectors: 2,
+      offlineConnectors: 1,
+    };
+  } else if (environmentId === 'staging') {
+    return {
+      brokerStatus: undefined,
+      dapsStatus: undefined,
+      loggingHouseStatus: undefined,
+      onlineConnectors: 0,
+      disturbedConnectors: 0,
+      offlineConnectors: 0,
+    };
+  } else {
+    return {
+      brokerStatus: up(),
+      dapsStatus: up(),
+      loggingHouseStatus: up(),
+      onlineConnectors: 1,
+      disturbedConnectors: 0,
+      offlineConnectors: 0,
+    };
+  }
 };
