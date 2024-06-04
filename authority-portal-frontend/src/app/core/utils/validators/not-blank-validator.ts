45d14967a2b5348df2f0ff427885f282e531ddc4
illfixit <66363651+illfixit@users.noreply.github.com>
2024-06-04 08:39:28 +0200

    chore: remove console.log

--- authority-portal-frontend/src/app/core/utils/validators/not-blank-validator.ts
+++ authority-portal-frontend/src/app/core/utils/validators/not-blank-validator.ts
@@ -2,11 +2,6 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
 export function notBlankValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    console.log(
-      control.value.length,
-      control.dirty,
-      control.value.trim() === '',
-    );
     if (control.value.length && control.dirty && control.value.trim() === '') {
       return {trimmedBlank: true};
     }
