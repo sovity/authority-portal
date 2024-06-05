1bfc796c1f50302c668d307d736fbf8a6628ed56
Kamil Czaja <kamil.czaja@sovity.de>
2024-06-05 15:19:14 +0200

    --wip-- [skip ci]

---
+++ authority-portal-backend/authority-portal-quarkus/src/main/kotlin/de/sovity/authorityportal/broker/BrokerServerResourceImpl.kt
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2023 sovity GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       sovity GmbH - initial API and implementation
+ *
+ */
+package de.sovity.authorityportal.broker
+
+import de.sovity.authorityportal.broker.api.BrokerServerResource
+import de.sovity.authorityportal.broker.api.model.AuthorityPortalConnectorDataOfferInfo
+import de.sovity.authorityportal.broker.api.model.CatalogPageQuery
+import de.sovity.authorityportal.broker.api.model.CatalogPageResult
+import de.sovity.authorityportal.broker.api.model.DataOfferDetailPageQuery
+import de.sovity.authorityportal.broker.api.model.DataOfferDetailPageResult
+import de.sovity.authorityportal.broker.services.api.AuthorityPortalConnectorDataOfferApiService
+import de.sovity.authorityportal.broker.services.api.CatalogApiService
+import de.sovity.authorityportal.broker.services.api.DataOfferDetailApiService
+import de.sovity.authorityportal.broker.services.config.AdminApiKeyValidator
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.transaction.Transactional
+
+/**
+ * Implementation of [BrokerServerResource]
+ */
+@ApplicationScoped
+class BrokerServerResourceImpl(
+    private val catalogApiService: CatalogApiService,
+    private val dataOfferDetailApiService: DataOfferDetailApiService,
+    private val adminApiKeyValidator: AdminApiKeyValidator,
+    private val authorityPortalConnectorDataOffersApiService: AuthorityPortalConnectorDataOfferApiService,
+) : BrokerServerResource {
+
+
+    @Transactional
+    override fun catalogPage(query: CatalogPageQuery): CatalogPageResult {
+        return catalogApiService.catalogPage(query)
+    }
+
+    @Transactional
+    override fun dataOfferDetailPage(query: DataOfferDetailPageQuery): DataOfferDetailPageResult {
+        return dataOfferDetailApiService.dataOfferDetailPage(query)
+    }
+
+    @Transactional
+    override fun getConnectorDataOffers(
+        endpoints: List<String>,
+        adminApiKey: String
+    ): MutableList<AuthorityPortalConnectorDataOfferInfo> {
+        adminApiKeyValidator.validateAdminApiKey(adminApiKey)
+        return authorityPortalConnectorDataOffersApiService.getConnectorDataOffersByEndpoints(endpoints)
+    }
+}
