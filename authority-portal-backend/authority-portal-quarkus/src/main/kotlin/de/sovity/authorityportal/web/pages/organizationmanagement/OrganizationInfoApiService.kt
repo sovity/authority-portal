19b3c3b3bedc19b5dd73f4c39dc7bb1592cfb660
Kamil Czaja <kamil.czaja@sovity.de>
2024-07-09 16:45:13 +0200

    fix: nullability

--- authority-portal-backend/authority-portal-quarkus/src/main/kotlin/de/sovity/authorityportal/web/pages/organizationmanagement/OrganizationInfoApiService.kt
+++ authority-portal-backend/authority-portal-quarkus/src/main/kotlin/de/sovity/authorityportal/web/pages/organizationmanagement/OrganizationInfoApiService.kt
@@ -124,7 +124,7 @@ class OrganizationInfoApiService(
     private fun getOrganizationDetailsDto(mdsId: String): OrganizationDetailsDto {
         val organization = organizationService.getOrganizationOrThrow(mdsId)
         val organizationAdmin = organization.createdBy?.let { userDetailService.getUserData(it) }
-        val legalIdType = organization.legalIdType.toDto()
+        val legalIdType = organization.legalIdType?.toDto()
         val memberList = userDetailService.getOrganizationMembers(mdsId)
 
         return OrganizationDetailsDto(
